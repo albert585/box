549237e7e53e780e9dc4a3cbb7299c4a3f0fd3e0
Albert585 <nnnn565@163.com>
2026-07-13 16:08:10 +0800

    更新README

--- 3rdparty/ffmpeg/libavutil/intmath.d
+++
@@ -1,6 +0,0 @@
-libavutil/intmath.o: libavutil/intmath.c libavutil/intmath.h config.h \
- libavutil/attributes.h libavutil/arm/intmath.h libavutil/attributes.h \
- libavutil/common.h libavutil/error.h libavutil/macros.h \
- libavutil/avconfig.h libavutil/version.h libavutil/internal.h \
- libavutil/libm.h libavutil/mathematics.h libavutil/rational.h \
- libavutil/intfloat.h
